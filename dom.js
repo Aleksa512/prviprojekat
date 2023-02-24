b44801bfd6e090b3d6ca58fb96a3014fe8cf3e0a
Aleksa512 <123654326+Aleksa512@users.noreply.github.com>
2023-02-24 17:35:27 +0100

    Add files via upload

--- dom.js
+++ dom.js
@@ -14,7 +14,7 @@ for(let i=0; i < listItems.length; i++)
     listItems[i].style.color = "white";
     if(i%2==0)
     {
-        listItems[i].style.backgroundColor = "#7C1AC9";
+        listItems[i].style.backgroundColor = "yellow";
     }
     else listItems[i].style.backgroundColor = "#200E6F";
 }
@@ -33,5 +33,24 @@ listItems[3].textContent="Cetvrti";
 
 listItems[3].backgroundColor="red";
 
-document.querySelector("#random-element p").style.backgroundColor="#B0DEBA";
+document.querySelector("#random-element p").style.backgroundColor="yellow";
 document.querySelector("#element1 p").style.backgroundColor="red";
+
+let listitem=document.getElementById("items");
+
+console.log(items.parentNode);
+items.parentNode.style.backgroundColor="blue";
+
+console.log(items.parentElement);
+
+console.log(items.childNodes);
+items.childNodes[1].style.backgroundColor="brown";
+
+console.log(items.children);
+items.children[0].style.backgroundColor="brown";
+
+items.previousElementSibling.style.color="red";
+
+let drugiElement=document.getElementsByClassName("list-item")[1];
+
+drugiElement.parentElement.previousElementSibling.style.backgroundColor="green";
